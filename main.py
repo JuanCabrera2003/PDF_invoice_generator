3e5c6eac20ebfbe714315155ac7a7fad59f25f56
Juan Cabrera <yesuscabrera2003@gmail.com>
2025-03-06 13:23:01 -0600

    add header, total, price, company photo and name

--- main.py
+++ main.py
@@ -49,6 +49,25 @@ for filepath in filepaths:
       pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1) 
       pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1) 
 
+    #fila del total
+    total = dt["total_price"].sum()
+
+    pdf.set_font(family="Times", size=10) 
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1) 
+    pdf.cell(w=70, h=8, txt="", border=1) 
+    pdf.cell(w=30, h=8, txt="", border=1) 
+    pdf.cell(w=30, h=8, txt="", border=1) 
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1) 
+
+    # se agrega una oracion conformando el total
+    pdf.set_font(family="Times", size=10, style="B") 
+    pdf.cell(w=30, h=8, txt=f"The total price is {total}", ln=1)
+
+    # se agrega el nombre de la company y la imagen 
+    pdf.set_font(family="Times", size=14, style="B") 
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     #imprimimos y guardamos el resultado en la carpeta pdfs 
     pdf.output(f"PDFs/{filename}.pdf")
